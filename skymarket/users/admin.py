5afd3308b1639220fb1e27fa43609351217cebfe
volodinakarina <118082835+volodinakarina@users.noreply.github.com>
2023-09-21 00:10:21 +0300

    Update admin.py

--- skymarket/users/admin.py
+++ skymarket/users/admin.py
@@ -1,10 +1,4 @@
-# from django.contrib import admin
-# from django.contrib.auth.admin import UserAdmin
-#
-# from .models import User
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 
 from .models import User
-# TODO Aдмика для пользователя - как реализовать ее можно подсмотреть в документаци django
-# TODO Обычно её всегда оформляют, но в текущей задачи делать её не обязательно
